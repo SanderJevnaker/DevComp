cc915bdb05b59ec49ce6627278af135ce759d918
SanderJevnaker <sander.jevnaker@adstate.com>
2023-09-14 22:17:14 +0200

    Initial Commit

---
+++ Springkiller/SpringkillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringkillerApp.swift
+//  Springkiller
+//
+//  Created by Sander Jevnaker on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpringkillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
